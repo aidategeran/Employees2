a1abae2235ada4d097a69c9ccdde9f188741b8a8
AidaTegeran <aidategeran@gmail.com>
2025-06-17 22:35:43 -0500

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String people = """
+                Flinstone , Fred, 1/1/1900
+                Rubble, Barney, 2/2/1905
+                Flinstone, Wilma, 3/3/1910
+                Rubble, Betty, 4/4/1915
+                """;
+
+        String regex =
+    }
+
+}
