2057fa38b25161f2fd8c156fb7720c7a139a8e05
AidaTegeran <aidategeran@gmail.com>
2025-06-21 22:29:52 -0500

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,58 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        String people = """
-            Flinstone, Fred, 1/1/1900, Programmer 
-            Rubble, Barney, 2/2/1905, Manager
-            Flinstone, Wilma, 3/3/1910, Analyst
-            Rubble, Betty, 4/4/1915, CEO
+        String peopleText = """
+            Flinstone, Fred, 1/1/1900, Programmer, {locpd=2000, yoe=10, iq=140} 
+            Flinstone2, Fred, 1/1/1900, Programmer,{locpd=1300, yoe=14, iq=100}
+            Flinstone3, Fred, 1/1/1900, Programmer,{locpd=2300, yoe=8, iq=105}
+            Flinstone4, Fred, 1/1/1900, Programmer,{locpd=1630, yoe=3, iq=115}
+            Flinstone5, Fred, 1/1/1900, Programmer,{locpd=5, yoe=10, iq=100}
+            Rubble, Barney, 2/2/1905, Manager, {orgSize=300, dr=10}
+            Rubble2, Barney, 2/2/1905, Manager, {orgSize=300, dr=4}
+            Rubble3, Barney, 2/2/1905, Manager, {orgSize=300, dr=2}
+            Rubble4, Barney, 2/2/1905, Manager, {orgSize=300, dr=8}
+            Rubble5, Barney, 2/2/1905, Manager, {orgSize=300, dr=20}
+            Flinstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
+            Flinstone2, Wilma, 3/3/1910, Analyst, {projectCount=4}
+            Flinstone3, Wilma, 3/3/1910, Analyst, {projectCount=5}
+            Flinstone4, Wilma, 3/3/1910, Analyst, {projectCount=6}
+            Flinstone5, Wilma, 3/3/1910, Analyst, {projectCount=9}
+            Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
             """;
 
-        String regex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}), \\s*(?<role>\\w+)\\n";
-        Pattern pat = Pattern.compile(regex);
-        Matcher mat = pat.matcher(people);
+        String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}), \\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?\\n";
+        Pattern peoplePat = Pattern.compile(peopleRegex);
+        Matcher peopleMat = peoplePat.matcher(peopleText);
+
+
+        String progRegex = "\\w+=(?<locpd>\\w),\\w+=(?<yoe>\\w),\\w+=(?<iq>\\w)";
+        Pattern coderPat = Pattern.compile(progRegex);
 
 
         int totalSalaries = 0;
-        while (mat.find()) {
-            totalSalaries += switch (mat.group("role")) {
-                case "Programmer" -> 3000;
-                case "Manager" -> 3500;
-                case "Analyst" -> 2500;
-                case "CEO" -> 5000;
+        while (peopleMat.find()) {
+            totalSalaries += switch (peopleMat.group("role")) {
+                case "Programmer" -> {
+                    String details = peopleMat.group("details");
+                    System.out.println(details);
+                    Matcher coderMat = coderPat.matcher(details);
+                    if (coderMat.find()) {
+                        String locpd = coderMat.group("locpd");
+                        String yoe = coderMat.group("yoe");
+                        String iq = coderMat.group("iq");
+                        System.out.printf("Programmer loc: %s yoe: %s iq: %s%n", locpd, yoe, iq);
+                    }
+                    yield 3000;
+                }
+                case "Manager" -> {
+                    yield 3500;
+                }
+                case "Analyst" -> {
+                    yield 2500;
+                }
+                case "CEO" -> {
+                    yield 5000;
+                }
                 default -> 0;
             };
         }
